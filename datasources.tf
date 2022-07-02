c1619a0735cff0ece44d4810bea09b12fb322073
Ifeanyi Glory <ifeanyigloryndukwe@gmail.com>
2022-07-02 17:35:09 +1200

    added the datasources,tf

---
+++ datasources.tf
@@ -0,0 +1,13 @@
+# create ubutu ec2 instance
+data "aws_ami" "server_ami"{
+    most_recent      = true
+    # you can find this id from the 
+    # ec2 server you choos to use 
+    # we are using ubuntu 
+    owners = ["099720109477"]
+    filter {
+    name   = "name"
+    #ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-arm64-server-20220610
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-arm64-server-*"]
+  }
+}
